656c931f8d707668d2399a0acdbe764cd9642e35
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-08-11 12:02:00 +0700

    feat(bricks): add `model` template

---
+++ bricks/model/__brick__/lib/datas/models/{{model_name.snakeCase()}}_model.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+final class {{model_name.pascalCase()}}Model extends Equatable {
+  const {{model_name.pascalCase()}}Model();
+
+  @override
+  List<Object?> get props => [];
+}
